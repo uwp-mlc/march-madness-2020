6cee796c06a7e40ef69ca4a90e7c319f067ba820
Nick Buchert <nicklt80@gmail.com>
2020-02-01 12:43:24 -0600

    Base file structure added
    Contains sample dummy code to throw away.

---
+++ Modules/data_manager.py
@@ -0,0 +1,8 @@
+class DataManager :
+    '''
+    Configure sample data
+    '''
+    def __init__(self):
+        self.X = [1, 2, 3, 4, 5]
+        self.Y = [1, 4, 9, 16, 25]
+        self.title = "Sample Plot"
